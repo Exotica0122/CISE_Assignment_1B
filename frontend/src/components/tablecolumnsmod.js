b973644f1b4d391c68878e56239ed0389cd05ff5
Enrico Serrano <enricoserrano13@gmail.com>
2022-09-24 13:15:01 +1200

    Added extra column for moderator

---
+++ frontend/src/components/tablecolumnsmod.js
@@ -0,0 +1,40 @@
+const updateHandler = () => {
+    alert("Plz update");
+};
+
+const tablecolumnsmod = [
+    {
+        Header: "Title",
+        accessor: "title",
+    },
+    {
+        Header: "Authors",
+        accessor: "authors",
+    },
+    {
+        Header: "Source",
+        accessor: "source",
+    },
+    {
+        Header: "Pub. Year",
+        accessor: "pubyear",
+    },
+    {
+        Header: "DOI",
+        accessor: "doi",
+    },
+    {
+        Header: "Claimed Benefit",
+        accessor: "claim",
+    },
+    {
+        Header: "Level of Evidence",
+        accessor: "evidence",
+    },
+    {
+        Header: "Action",
+        accessor: "action",
+    },
+];
+
+export default tablecolumnsmod;
