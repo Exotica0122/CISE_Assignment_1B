4c71e9b828355e3960cb1b402f31b15e2bc841c4
yennie0819 <anpeteran2@gmail.com>
2022-09-25 23:01:26 +1300

    Search Bar Added

--- frontend/src/pages/SE-Practice.js
+++ frontend/src/pages/SE-Practice.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
 import Dropdown from "../components/Dropdown.js";
 import ArticlesTable from "../components/ArticlesTable";
+import SearchIcon from "@mui/icons-material/Search";
+import { TextField, InputAdornment, IconButton } from "@mui/material";
 
 import axios from "axios";
 
 const SEPractice = () => {
   const [articles, setArticles] = useState([]);
   const [practice, setPractice] = useState("");
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     axios
@@ -35,10 +38,30 @@ const SEPractice = () => {
       <p>Loading articles</p>
     );
 
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <div>
       <h2>Select SE Practice to get evidence for the claimed benefits</h2>
       <Dropdown setPractice={setPractice} />
+      <TextField
+        value={search}
+        onChange={handleSearchChange}
+        margin="normal"
+        style={{ textAlign: "right", float: "right" }}
+        label="Search Article"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment>
+              <IconButton>
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
       {dataColumn}
     </div>
   );
