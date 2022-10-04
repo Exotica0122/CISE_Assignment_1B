e826fdb9b90938ee819603d87d057ea7016394cf
yennie0819 <anpeteran2@gmail.com>
2022-10-04 17:52:13 +1300

    Navbar Changed to RRD Links

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,97 +1,114 @@
-import {useState} from "react";
+import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 
 const pages = [
-  { title: "Home", link: "/" },
-  { title: "Select the Practice", link: "/SEPractice" },
-  { title: "Submit an Article", link: "/SubmitArticle" },
-  { title: "Moderator Page", link: "/admin" },
-  { title: "Analyst Page", link: "/analyst" },
+    { title: "Home", link: "/" },
+    { title: "Select the Practice", link: "/SEPractice" },
+    { title: "Submit an Article", link: "/SubmitArticle" },
+    { title: "Moderator Page", link: "/admin" },
+    { title: "Analyst Page", link: "/analyst" },
 ];
 
 const NavBar = () => {
-  const [anchorElNav, setAnchorElNav] = useState(null);
+    const [anchorElNav, setAnchorElNav] = useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-  return (
-    <AppBar position="static" style={{ background: '#2E3B55' }}>
-      <Container maxWidth="x1">
-        <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page.title} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">
-                    <Link
-                      style={{ textDecoration: "none", color: "black" }}
-                      to={`${page.link}`}
+    return (
+        <AppBar position="static" style={{ background: "#2E3B55" }}>
+            <Container maxWidth="x1">
+                <Toolbar disableGutters>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "flex", md: "none" },
+                        }}
                     >
-                      {page.title}
-                    </Link>
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <MenuItem key={page.title} onClick={handleCloseNavMenu}>
-                <Typography textAlign="center">
-                  <Link
-                    style={{ textDecoration: "none", color: "white" }}
-                    to={`${page.link}`}
-                  >
-                    {page.title}
-                  </Link>
-                </Typography>
-              </MenuItem>
-            ))}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "left",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "left",
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: "block", md: "none" },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <Button
+                                    key={page.title}
+                                    onClick={handleCloseNavMenu}
+                                    sx={{
+                                        my: 2,
+                                        mx: 2,
+                                        color: "white",
+                                        display: "block",
+                                    }}
+                                    component={Link}
+                                    to={page.link}
+                                >
+                                    {page.title}
+                                </Button>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "none", md: "flex" },
+                        }}
+                    >
+                        {pages.map((page) => (
+                            <Button
+                                key={page.title}
+                                onClick={handleCloseNavMenu}
+                                sx={{
+                                    my: 2,
+                                    mx: 2,
+                                    color: "white",
+                                    display: "block",
+                                }}
+                                component={Link}
+                                to={page.link}
+                            >
+                                {page.title}
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 };
 export default NavBar;
