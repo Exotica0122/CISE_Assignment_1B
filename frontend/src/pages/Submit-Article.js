db2594da5864eaae70c6344b661e6da3645dda24
Enrico Serrano <enricoserrano13@gmail.com>
2022-10-10 14:24:41 +1300

    Centered headings

--- frontend/src/pages/Submit-Article.js
+++ frontend/src/pages/Submit-Article.js
@@ -1,9 +1,9 @@
 import React from "react";
 import SubmissionForm from "../components/SubmissionForm";
-
+import '../index.css';
 const SubmitArticle = () => {
     return (
-        <div>
+        <div className="content-center">
             <h2>Submit Article</h2>
             <p>
                 This will be a form to allow submitters to submit an article for
