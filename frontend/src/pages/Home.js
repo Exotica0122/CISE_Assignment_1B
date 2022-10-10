0eac83426376f05cfe06567d7573fa62622d5fbe
Enrico Serrano <enricoserrano13@gmail.com>
2022-10-10 14:20:36 +1300

    Updated style for submissionForm

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,18 @@
 import React from "react";
- 
-const Home = () =>  {
-    return (
-      <div>
-        <h2>CISE Assignment 1B SPEED App</h2>
-        <p>I am not sure what to put on the home page</p>
-        <p>A nice logo? Instructions?</p>
-      </div>
-    );
-  }
- 
+
+const Home = () => {
+  return (
+    <div>
+      <h2>CISE Assignment 1B SPEED App</h2>
+      <h3>ENSE701/51 - Group 3</h3>
+      <h4>Team Members</h4>
+      <ul>
+        <li>Peter An</li>
+        <li>Enrico Serrano</li>
+        <li>Leandro Machado</li>
+      </ul>
+    </div>
+  );
+}
+
 export default Home;
