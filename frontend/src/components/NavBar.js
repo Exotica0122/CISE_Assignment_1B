5aa3dc1352e476467d7efcb5f6789b276efcc356
Enrico Serrano <enricoserrano13@gmail.com>
2022-10-17 13:00:24 +1300

    Removed unnecessary files and redesigned login form

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -19,7 +19,7 @@ const pages = [
     { title: "Submit Article", link: "/SubmitArticle", type: 'all' },
     { title: "Moderator Page", link: "/admin", type: 'moderator', qty: 0 },
     { title: "Analyst Page", link: "/analyst", type: 'analyst', qty: 0 },
-    { title: "Login Page", link: "/TestLoginForm", type: 'all' },
+    { title: "Login Page", link: "/LoginForm", type: 'all' },
 ];
 
 
@@ -172,89 +172,6 @@ const NavBar = ({ currentUser, qtyPendingItems }) => {
                 </Toolbar>
             </Container>
         </AppBar>
-        /*
-        <AppBar position="static" style={{ background: "#2E3B55" }}>
-            <Container maxWidth="x1">
-                <Toolbar disableGutters>
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "flex", md: "none" },
-                        }}
-                    >
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "left",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "left",
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: "block", md: "none" },
-                            }}
-                        >
-                            {pages.map((page) => (
-                                <Button
-                                    key={page.title}
-                                    onClick={handleCloseNavMenu}
-                                    sx={{
-                                        my: 2,
-                                        mx: 2,
-                                        color: "white",
-                                        display: "block",
-                                    }}
-                                    component={Link}
-                                    to={page.link}
-                                >
-                                    {page.title}
-                                </Button>
-                            ))}
-                        </Menu>
-                    </Box>
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "none", md: "flex" },
-                        }}
-                    >
-                        {pages.map((page) => (
-                            <Button
-                                key={page.title}
-                                onClick={handleCloseNavMenu}
-                                sx={{
-                                    my: 2,
-                                    mx: 2,
-                                    color: "white",
-                                    display: "block",
-                                }}
-                                component={Link}
-                                to={page.link}
-                            >
-                                {page.title}
-                            </Button>
-                        ))}
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
-        */
     );
 };
 export default NavBar;
